460725c3ae9cb5c0f9f193eda86e4319127508be
eyihcn <526048537@qq.com>
2017-02-23 18:53:37 +0800

    2016

--- src/concurrent/demo/ProducerConsumerDemo2.java
+++ src/concurrent/demo/ProducerConsumerDemo2.java
@@ -1,29 +1,29 @@
 package concurrent.demo;
 
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 public class ProducerConsumerDemo2 {
 
 	public static void main(String[] args) {
-		StackBasket s = new StackBasket(100);
-		new Thread(new Producer(s), "===> pro-1").start();
-		new Thread(new Producer(s), "===> pro-2").start();
-		new Thread(new Consumer(s), "+++> con-1").start();
-		new Thread(new Consumer(s), "+++> con-2").start();
+		BlockingQueue<Product2> arrayBlockingQueue = new ArrayBlockingQueue<Product2>(100);
+		int tobeProduceNum = 100;
+		new Thread(new Producer2(arrayBlockingQueue, tobeProduceNum), "===> pro-1").start();
+		new Thread(new Producer2(arrayBlockingQueue, tobeProduceNum), "===> pro-2").start();
+		new Thread(new Consumer2(arrayBlockingQueue), "+++> con-1").start();
+		new Thread(new Consumer2(arrayBlockingQueue), "+++> con-2").start();
 		System.out.println("main Thread over!!!");
 	}
 }
 
-class Product { // 产品
+class Product2 { // 产品
 	private int id;// 产品编号
 
-	public Product() {
+	public Product2() {
 		super();
 	}
 
-	public Product(int id) {
+	public Product2(int id) {
 		super();
 		this.id = id;
 	}
@@ -37,90 +37,32 @@ class Product { // 产品
 	}
 }
 
-class StackBasket {
+class Producer2 implements Runnable {
 
-	Lock lock = new ReentrantLock();
-	Condition proCondition = lock.newCondition();
-	Condition conCondition = lock.newCondition();
+	BlockingQueue<Product2> blockingQueue = null;
+	int tobeProduceNum;// 每个生产者可以生成的数量
 
-	Product[] products = null;
-	int len;
-
-	public StackBasket(int size) {
-		if (size < 1) {
-			size = 10;
-		}
-		System.out.println("StackBasket init size= " + size);
-		products = new Product[size];
-	}
-
-	public StackBasket() {
-		this(0);
-	}
-
-	public Product pop() throws InterruptedException { // 消费
-		lock.lock();
-		// 消息的打印 和 len值的改变
-		try {
-			// 当篮子里没有 产品时，所有进来的消费者线程都要等待，且释放cpu的执行权
-			while (0 == len) {
-				System.out.println("======仓库是空的====，无法消费: " + Thread.currentThread().getName() + " 等待");
-				proCondition.await();
-			}
-			System.out.println(Thread.currentThread().getName() + " 消费了一个产品(id= " + products[len - 1].getId()
-					+ "), 仓库里面还有 " + (len - 1) + " 个产品");
-			// 此时len递减，但在len递减之后，products数组仍然持有len位置上Product实例的引用，
-			// 且在程序运行结束之前可能不会被释放，则可能导致内存泄漏，为了防止内存泄漏，出栈后将对应栈的位置元素置null
-			Product p = products[--len];
-			products[len] = null;
-			// 只唤醒 通过conCongdition.await()冻结的线程，不会唤醒生产线程
-			conCondition.signalAll();
-			return p;
-		} finally {
-			lock.unlock();
-		}
-	}
-
-	public void push(Product product) throws InterruptedException {// 生产
-		lock.lock();
-		try {
-			while (len == products.length) {
-				System.out.println("++++++++仓库已经满了++++++，无法继续生产: " + Thread.currentThread().getName() + " 等待");
-				conCondition.await();
-			}
-			System.out.println(Thread.currentThread().getName() + "生产了一个产品（id= " + product.getId() + ",  仓库里面还有 "
-					+ (len + 1) + " 个产品");
-			products[len++] = product;
-			// 一旦生产一个产品，就唤醒所有的消费者
-			proCondition.signalAll();
-		} finally {
-			lock.unlock();
-		}
-	}
-}
-
-class Producer implements Runnable {
-
-	StackBasket stackBasket = null;
-
-	public Producer() {
+	public Producer2() {
 		super();
 	}
 
-	public Producer(StackBasket stackBasket) {
+	public Producer2(BlockingQueue<Product2> blockingQueue, int tobeProduceNum) {
 		super();
-		this.stackBasket = stackBasket;
+		this.blockingQueue = blockingQueue;
+		this.tobeProduceNum = tobeProduceNum;
 	}
 
 	@Override
 	public void run() {
-		// 生产100个产品
-		Product product = null;
-		for (int index = 1; index < 50; index++) {
-			product = new Product(index);
+		Product2 Product2 = null;
+		// 生产者不停的生产
+		for (int index = 0; index < tobeProduceNum; index++) {
+			Product2 = new Product2(index);
 			try {
-				stackBasket.push(product);
-			} catch (InterruptedException e) {
+				// 将指定的元素插入到该队列中，如果需要的话，等待空间可用
+				blockingQueue.put(Product2);
+
+			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
@@ -129,25 +71,26 @@ class Producer implements Runnable {
 
 }
 
-class Consumer implements Runnable {
+class Consumer2 implements Runnable {
 
-	StackBasket stackBasket = null;
+	BlockingQueue<Product2> blockingQueue = null;
 
-	public Consumer() {
+	public Consumer2() {
 		super();
 	}
 
-	public Consumer(StackBasket stackBasket) {
+	public Consumer2(BlockingQueue<Product2> blockingQueue) {
 		super();
-		this.stackBasket = stackBasket;
+		this.blockingQueue = blockingQueue;
 	}
 
 	@Override
 	public void run() {
 
-		for (int index = 1; index < 100; index++) {
+		for (;;) {
 			try {
-				stackBasket.pop();
+				// 检索并移除该队列的头，在必要时等待，直到元素变为可用。
+				blockingQueue.take();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
